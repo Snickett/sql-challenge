f9ce70f2277be5f22231bef0e7d4ebed56525b22
Justin <duckweiser@gmail.com>
2020-10-08 15:34:05 -0500

    Queries complete

--- EmployeeSQL/query_work.sql
+++ EmployeeSQL/query_work.sql
@@ -22,17 +22,46 @@ from part_1
 INNER JOIN employees ON
 	 part_1.emp_no = employees.emp_no
 
-
 -- List the department of each employee with the following information: employee number, last name, first name, and department name.
+create VIEW part_2 AS
+select employees.emp_no, employees.last_name, employees.first_name
+from employees
+--limit 5
 
+create VIEW part_3 AS
+select dept_emp.emp_no, departments.dept_name
+from dept_emp
+INNER JOIN departments on
+	dept_emp.dept_no = departments.dept_no
+--limit 5
+
+select part_2.emp_no, part_2.last_name, part_2.first_name, part_3.dept_name
+from part_2
+INNER JOIN part_3 on
+	part_3.emp_no = part_2.emp_no
+--limit 5;
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
-
+select first_name, last_name, sex
+from employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%'
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
-
+select part_2.emp_no, part_2.last_name, part_2.first_name, part_3.dept_name
+from part_2
+INNER JOIN part_3 on
+	part_3.emp_no = part_2.emp_no
+WHERE dept_name = 'Sales'
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
-
+select part_2.emp_no, part_2.last_name, part_2.first_name, part_3.dept_name
+from part_2
+INNER JOIN part_3 on
+	part_3.emp_no = part_2.emp_no
+WHERE dept_name = 'Sales' or dept_name = 'Development'
 
 -- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+select last_name, count (last_name) as "last name count"
+from employees
+GROUP BY last_name
+Order by "last name count" DESC
